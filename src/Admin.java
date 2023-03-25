5d93f873b4e5ebca5238cc6bd2b7402480e565b8
jdww5567 <jadaniel06@gmail.com>
2023-03-25 13:13:06 -0500

    Basic layout of user and admin classes

---
+++ src/Admin.java
@@ -0,0 +1,41 @@
+public class Admin extends User{
+    private boolean admin;
+
+    /*
+     * Constructor that just invokes User constructor
+     */
+    public Admin(String username, String password) {
+        super(username, password);
+        this.admin = false;
+    }
+
+    /*
+     * check if the admin is listed as admin in database
+     */
+    public boolean checkAdmin() {
+        boolean found = false;
+
+        // look through xml data for admin flag on this user, set found
+
+        if (found) {
+            this.admin = true;
+        }
+
+        return found;
+    }
+
+    /*
+     * remove a user from xml data
+     */
+    public boolean deleteUser(String username) {
+        boolean found = false;
+
+        // search for username in data, set found
+
+        if (found) {
+            // edit xml to completely remove user
+        }
+
+        return found;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,128 @@
+public class User {
+    private String username;
+    private String password;
+    private boolean loggedIn;
+    // private ArrayList<Collection> collections;
+
+
+    /*
+     * default constructor
+     */
+    public User() {
+        this.username = null;
+        this.password = null;
+        this.loggedIn = false;
+        // collections = new ArrayList<>();
+    }
+
+    /*
+     * Creates User at login attempt
+     */
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false;
+        // collections = new ArrayList<>();
+    }
+
+    /*
+     * Checks if login attempt is a valid user in database (couldn't we make this part of the constructor?)
+     */
+    public boolean checkUser() {
+        boolean found = false;
+
+        // check xml data and update found
+
+        if (found) {
+            this.loggedIn = true;
+            // for collections in database owned by user, this.collections.add(collection)
+        }
+
+        return found;
+    }
+
+    /*
+     * I don't think this method should exist. The login page or underlying class should write to the database.
+     */
+    public void writeUser() {
+        // write this.username to database
+        // write this.password to database
+    }
+
+    /*
+     * Not sure how much this method should interact directly with the database
+     */
+    public void logoutUser() {
+        // write new or edited collections to database
+        this.loggedIn = false;
+    }
+
+    /*
+     * get the username; likely for the purpose of reviews
+     */
+    public String getUser() {
+        return this.username;
+    }
+
+    /*
+     * set the username; alternative method for setting up a user at log in
+     */
+    public boolean setUser(String username) {
+        boolean valid = false;
+
+        // check if username is a valid String, set valid
+
+        if (valid) {
+            this.username = username;
+        }
+
+        return valid;
+    }
+
+    /*
+     * get the password; possibly used by login page for writing to the database or checking user against the database
+     */
+    public String getPassword() {
+        return this.password;
+    }
+
+    /*
+     * set the password; alternative method for setting up a user at log in
+     */
+    public boolean setPassword(String password) {
+        boolean valid = false;
+
+        // check if password is valid String, set valid
+
+        if (valid) {
+            this.password = password;
+        }
+
+        return valid;
+    }
+
+    /*
+     * Add a collection to user and database
+     */
+    // public void addCollection(Collection collection) {
+    //    this.collections.add(collection);
+    //    // write collection to database, possibly done inside collection class or collection page class instead
+    // }
+
+    /*
+     * Delete collection
+     */
+    // public boolean deleteCollection(String id) {
+    //    // boolean found = false;
+    //    // int index = 0;
+    //
+    //    // loop through collections to find the relevant one, set found and index
+    //
+    //    // if (found) {
+    //          // this.collections.remove(index)
+    //          // deletion in xml will be handled by a different class
+    //    // }
+    //
+    //    // return found;
+    // }
+}
