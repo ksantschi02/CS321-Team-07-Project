8cf4a9b10d38351f8028adbdac50649b254b73a4
ksantschi02 <kjs0033@uah.edu>
2023-04-15 19:25:11 -0500

    Pushes Database class

--- src/Database.java
+++ src/Database.java
@@ -9,8 +9,7 @@ public class Database {
     private ArrayList<User> userList;
     private int gameCounter;
 
-    public void readGameList()
-    {
+    public void readGameList() {
         ArrayList<Game> returnList = new ArrayList<>();
         try {
             File inputFile = new File("C:/temp/gamelist.xml");
@@ -19,20 +18,15 @@ public class Database {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
             NodeList nList = doc.getElementsByTagName("item");
-            for (int i = 0; i < nList.getLength(); i++)
-            {
+            for (int i = 0; i < nList.getLength(); i++) {
                 Node nNode = nList.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE)
-                {
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     NodeList childNodes = nNode.getChildNodes();
                     ArrayList<Review> reviews = new ArrayList<>();
-                    for (int j = 0; j < childNodes.getLength(); j++)
-                    {
+                    for (int j = 0; j < childNodes.getLength(); j++) {
                         Node item = childNodes.item(j);
-                        if (item.getNodeType() == Node.ELEMENT_NODE)
-                        {
-                            if ("review".equalsIgnoreCase(item.getNodeName()))
-                            {
+                        if (item.getNodeType() == Node.ELEMENT_NODE) {
+                            if ("review".equalsIgnoreCase(item.getNodeName())) {
                                 Element rElement = (Element) item;
                                 Review r = new Review(
                                         Double.parseDouble(((Element) rElement.getElementsByTagName("rating").item(0)).getAttribute("value")),
@@ -45,11 +39,9 @@ public class Database {
                     }
                     Element eElement = (Element) nNode;
                     double rating;
-                    if (((Element) eElement.getElementsByTagName("avgrating").item(0)) == null)
-                    {
+                    if ((eElement.getElementsByTagName("avgrating").item(0)) == null) {
                         rating = 0;
-                    }
-                    else {
+                    } else {
                         rating = Double.parseDouble(((Element) eElement.getElementsByTagName("avgrating").item(0)).getAttribute("value"));
                     }
                     Game e = new Game(Integer.parseInt(eElement.getAttribute("id")),
@@ -75,20 +67,16 @@ public class Database {
         this.gameList = returnList;
     }
 
-    public Game retrieveByName(String name)
-    {
-        for (Game game : gameList)
-        {
-            if (game.getTitle().equalsIgnoreCase(name))
-            {
+    public Game retrieveByName(String name) {
+        for (Game game : gameList) {
+            if (game.getTitle().equalsIgnoreCase(name)) {
                 return game;
             }
         }
         return null;
     }
 
-    public void readUserList()
-    {
+    public void readUserList() {
         ArrayList<User> returnList = new ArrayList<>();
         try {
             File inputFile = new File("C:/temp/userlist.xml");
@@ -97,29 +85,23 @@ public class Database {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
             NodeList nList = doc.getElementsByTagName("user");
-            for (int i = 0; i < nList.getLength(); i++)
-            {
+            for (int i = 0; i < nList.getLength(); i++) {
                 Node nNode = nList.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE)
-                {
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
                     User u = new User(
-                            ((Element)eElement.getElementsByTagName("username").item(0)).getAttribute("value"),
-                            ((Element)eElement.getElementsByTagName("password").item(0)).getAttribute("value")
+                            ((Element) eElement.getElementsByTagName("username").item(0)).getAttribute("value"),
+                            ((Element) eElement.getElementsByTagName("password").item(0)).getAttribute("value")
                     );
                     NodeList collectionList = ((Element) nNode).getElementsByTagName("collection");
-                    for (int j = 0; j < collectionList.getLength(); j++)
-                    {
+                    for (int j = 0; j < collectionList.getLength(); j++) {
                         Node cNode = collectionList.item(j);
-                        if (cNode.getNodeType() == Node.ELEMENT_NODE)
-                        {
+                        if (cNode.getNodeType() == Node.ELEMENT_NODE) {
                             ArrayList<Game> games = new ArrayList<>();
                             NodeList cGames = cNode.getChildNodes();
-                            for (int k = 0; k < cGames.getLength(); k++)
-                            {
+                            for (int k = 0; k < cGames.getLength(); k++) {
                                 Node gNode = cGames.item(k);
-                                if (gNode.getNodeType() == Node.ELEMENT_NODE)
-                                {
+                                if (gNode.getNodeType() == Node.ELEMENT_NODE) {
                                     Element gElement = (Element) gNode;
                                     games.add(retrieveByName(gElement.getAttribute("name")));
                                 }
@@ -127,6 +109,7 @@ public class Database {
                             Element cElement = (Element) cNode;
                             Collection c = new Collection(
                                     Integer.parseInt(cElement.getAttribute("sortType")),
+                                    Integer.parseInt(cElement.getAttribute("filterType")),
                                     cElement.getAttribute("name"),
                                     games);
                             u.addCollection(c);
@@ -142,33 +125,32 @@ public class Database {
         this.userList = returnList;
     }
 
-    public User retrieveByUser(String username)
-    {
-        for (User user : userList)
-        {
-            if (user.getUser().equalsIgnoreCase(username))
-            {
+    public User retrieveByUser(String username) {
+        for (User user : userList) {
+            if (user.getUser().equalsIgnoreCase(username)) {
                 return user;
             }
         }
         return null;
     }
 
-    public void addUserScratch(String username, String password)
-    {
+    public void addUserScratch(String username, String password) {
         userList.add(new User(username, password));
     }
 
-    public void addUserObject(User user)
-    {
+    public void addUserObject(User user) {
         userList.add(user);
     }
 
-    public ArrayList<Game> getGameList() {return gameList;}
-    public ArrayList<User> getUserList() {return userList;}
+    public ArrayList<Game> getGameList() {
+        return gameList;
+    }
 
-    public void saveDatabase()
-    {
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void saveDatabase() {
         try {
             File inputFile = new File("C:/temp/gamelist.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -176,23 +158,17 @@ public class Database {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
             NodeList nList = doc.getElementsByTagName("item");
-            for (int i = 0; i < nList.getLength(); i++)
-            {
+            for (int i = 0; i < nList.getLength(); i++) {
                 Node nNode = nList.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE)
-                {
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     NodeList childNodes = nNode.getChildNodes();
-                    for (int j = 0; j < childNodes.getLength(); j++)
-                    {
+                    for (int j = 0; j < childNodes.getLength(); j++) {
                         Node item = childNodes.item(j);
-                        if (item.getNodeType() == Node.ELEMENT_NODE)
-                        {
-                            if ("avgrating".equalsIgnoreCase(item.getNodeName()))
-                            {
+                        if (item.getNodeType() == Node.ELEMENT_NODE) {
+                            if ("avgrating".equalsIgnoreCase(item.getNodeName())) {
                                 nNode.removeChild(item);
                             }
-                            if ("review".equalsIgnoreCase(item.getNodeName()))
-                            {
+                            if ("review".equalsIgnoreCase(item.getNodeName())) {
                                 nNode.removeChild(item);
                             }
                         }
@@ -202,8 +178,7 @@ public class Database {
                     avgrating.setAttribute("value", String.valueOf(gameList.get(i).getAvgRating()));
                     nNode.appendChild(avgrating);
 
-                    for (Review r : gameList.get(i).getReviews())
-                    {
+                    for (Review r : gameList.get(i).getReviews()) {
                         Element review = doc.createElement("review");
 
                         Element rating = doc.createElement("rating");
@@ -235,16 +210,17 @@ public class Database {
             doc.getDocumentElement().normalize();
             Node users = (doc.getElementsByTagName("users")).item(0);
             NodeList nList = users.getChildNodes();
-            for (User u : userList)
-            {
-                for (int i = 0; i < nList.getLength(); i++)
-                {
+            ArrayList<User> userListTemp = new ArrayList<>();
+            for (User u : userList) {
+                userListTemp.add(new User(u));
+            }
+            for (User u : userListTemp) {
+                for (int i = 0; i < nList.getLength(); i++) {
                     Node nNode = nList.item(i);
-                    if (nNode.getNodeType() == Node.ELEMENT_NODE)
-                    {
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element eElement = (Element) nNode;
-                        if (((Element)eElement.getElementsByTagName("username").item(0)).getAttribute("value") == u.getUser()) {
-                            if (((Element) eElement.getElementsByTagName("password").item(0)).getAttribute("value") == u.getPassword()) {
+                        if (((Element) eElement.getElementsByTagName("username").item(0)).getAttribute("value").equals(u.getUser())) {
+                            if (((Element) eElement.getElementsByTagName("password").item(0)).getAttribute("value").equals(u.getPassword())) {
                                 NodeList childNodes = nNode.getChildNodes();
                                 for (int j = 0; j < childNodes.getLength(); j++) {
                                     Node item = childNodes.item(j);
@@ -257,32 +233,49 @@ public class Database {
                                 // RENEW/ADD COLLECTIONS HERE
                                 for (Collection c : u.getCollections()) {
                                     Element collection = doc.createElement("collection");
-                                    //collection.setAttribute("sortType", c.get);
+                                    collection.setAttribute("sortType", String.valueOf(c.getCollectionSortType()));
+                                    collection.setAttribute("filterType", String.valueOf(c.getCollectionFilterType()));
+                                    collection.setAttribute("name", c.getTitle());
+                                    for (Game g : c.getGames()) {
+                                        Element game = doc.createElement("game");
+                                        game.setAttribute("name", g.getTitle());
+                                        collection.appendChild(game);
+                                    }
+                                    nNode.appendChild(collection);
                                 }
+                                userListTemp.remove(u);
                             }
                         }
-                    } //THIS SECTION IS NOT DONE
+                    }
                 }
+            }
+            for (User u : userListTemp) {
+                Element user = doc.createElement("user");
+
+                Element username = doc.createElement("username");
+                username.setAttribute("value", u.getUser());
+                user.appendChild(username);
+
+                Element password = doc.createElement("password");
+                password.setAttribute("value", u.getPassword());
+                user.appendChild(password);
+
+                for (Collection c : u.getCollections()) {
+                    Element collection = doc.createElement("collection");
+                    collection.setAttribute("sortType", String.valueOf(c.getCollectionSortType()));
+                    collection.setAttribute("filterType", String.valueOf(c.getCollectionFilterType()));
+                    collection.setAttribute("name", c.getTitle());
+                    for (Game g : c.getGames()) {
+                        Element game = doc.createElement("game");
+                        game.setAttribute("name", g.getTitle());
+                        collection.appendChild(game);
+                    }
+                    user.appendChild(collection);
+                }
+                users.appendChild(user);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-//    for (int k = 0; k < cGames.getLength(); k++)
-//    {
-//        Node gNode = cGames.item(k);
-//        if (gNode.getNodeType() == Node.ELEMENT_NODE)
-//        {
-//            Element gElement = (Element) gNode;
-//            games.add(retrieveByName(gElement.getAttribute("name")));
-//        }
-//    }
-//    Element cElement = (Element) cNode;
-//    Collection c = new Collection(
-//            Integer.parseInt(cElement.getAttribute("sortType")),
-//            cElement.getAttribute("name"),
-//            games);
-//                            u.addCollection(c);
 }
-
-
