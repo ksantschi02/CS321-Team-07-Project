d4281320a9baec35a4b361d0a0a815b832719496
jdww5567 <jadaniel06@gmail.com>
2023-04-19 08:33:56 -0500

    filter menu implementation

--- src/data_retrieval/Filter.java
+++ src/data_retrieval/Filter.java
@@ -30,40 +30,28 @@ public class Filter {
          if (filterType == 0) {}
          // min players
          else if (filterType == 1) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMinPlayers() > 1) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 2) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMinPlayers() > 2) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 3) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMinPlayers() > 3) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 4) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMinPlayers() > 4) {
                      collection.remove(i);
                  }
@@ -71,80 +59,56 @@ public class Filter {
          }
          // max players
          else if (filterType == 5) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (collection.get(i).getMaxPlayers() != 1) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (collection.get(i).getMaxPlayers() < 1) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 6) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 2) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 7) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 3) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 8) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 4) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 9) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 5) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 10) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 6) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 11) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 7) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 12) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
+             for (int i = collection.size() - 1; i >= 0; i--) {
                  if (collection.get(i).getMaxPlayers() < 8) {
                      collection.remove(i);
                  }
@@ -152,112 +116,121 @@ public class Filter {
          }
          // min age
          else if (filterType == 13) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (collection.get(i).getMaxPlayers() > 3) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (collection.get(i).getMinAge() > 3) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 14) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (collection.get(i).getMaxPlayers() > 7) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (collection.get(i).getMinAge() > 7) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 15) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (collection.get(i).getMaxPlayers() > 12) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (collection.get(i).getMinAge() > 12) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 16) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (collection.get(i).getMaxPlayers() > 16) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (collection.get(i).getMinAge() > 16) {
                      collection.remove(i);
                  }
              }
          }
          // genre
          else if (filterType == 17) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("family")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Card Game")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 18) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("dexterity")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Science Fiction")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 19) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("party")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Economic")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 20) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("abstract")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Adventure")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 21) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("thematic")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Educational")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 22) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
-                 }
-                 if (!collection.get(i).getGenre().equals("euro")) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Deduction")) {
                      collection.remove(i);
                  }
              }
          }
          else if (filterType == 23) {
-             for (int i = 0; i < collection.size(); i++) {
-                 if (i >= collection.size()) {
-                     break;
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Exploration")) {
+                     collection.remove(i);
                  }
-                 if (!collection.get(i).getGenre().equals("war")) {
+             }
+         }
+         else if (filterType == 24) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Animals")) {
+                     collection.remove(i);
+                 }
+             }
+         }
+         else if (filterType == 25) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Movies / TV / Radio Theme")) {
+                     collection.remove(i);
+                 }
+             }
+         }
+         else if (filterType == 26) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Fantasy")) {
+                     collection.remove(i);
+                 }
+             }
+         }
+         else if (filterType == 27) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Fighting")) {
+                     collection.remove(i);
+                 }
+             }
+         }
+         else if (filterType == 28) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Civilization")) {
+                     collection.remove(i);
+                 }
+             }
+         }
+         else if (filterType == 29) {
+             for (int i = collection.size() - 1; i >= 0; i--) {
+                 if (!collection.get(i).getGenre().contains("Puzzle")) {
                      collection.remove(i);
                  }
              }
--- src/game_info/SortCollection.java
+++ src/game_info/SortCollection.java
@@ -22,11 +22,11 @@ public class SortCollection implements Comparator<Game>{
         else if (index == 1) {return -Double.compare(gameA.getAvgRating(), gameB.getAvgRating());}
         else if (index == 2) {return Double.compare(gameA.getAvgRating(), gameB.getAvgRating());}
         // title
-        else if (index == 3) {return -gameA.getTitle().compareToIgnoreCase(gameB.getTitle());}
-        else if (index == 4) {return gameA.getTitle().compareToIgnoreCase(gameB.getTitle());}
+        else if (index == 3) {return gameA.getTitle().compareToIgnoreCase(gameB.getTitle());}
+        else if (index == 4) {return -gameA.getTitle().compareToIgnoreCase(gameB.getTitle());}
         // average of min and max playtime
-        else if (index == 5) {return -Double.compare(((double)gameA.getMinPlaytime() + (double)gameA.getMaxPlaytime()) / 2.0, ((double)gameB.getMinPlaytime() + (double)gameB.getMaxPlaytime()) / 2.0);}
-        else if (index == 6) {return Double.compare(((double)gameA.getMinPlaytime() + (double)gameA.getMaxPlaytime()) / 2.0, ((double)gameB.getMinPlaytime() + (double)gameB.getMaxPlaytime()) / 2.0);}
+        else if (index == 5) {return Double.compare(((double)gameA.getMinPlaytime() + (double)gameA.getMaxPlaytime()) / 2.0, ((double)gameB.getMinPlaytime() + (double)gameB.getMaxPlaytime()) / 2.0);}
+        else if (index == 6) {return -Double.compare(((double)gameA.getMinPlaytime() + (double)gameA.getMaxPlaytime()) / 2.0, ((double)gameB.getMinPlaytime() + (double)gameB.getMaxPlaytime()) / 2.0);}
         else {return 0;}
     }
 }
